aaa3ddd9684d8b8c0ab2b31a25c0da705961fcff
vitorueno <vitoruenootto@gmail.com>
2024-11-27 02:31:53 -0300

    chore: add test automation script

--- experimento.sh
+++ experimento.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-mkdir -p experimento2/
+dir="experimento_refactor/"
+mkdir -p $dir
 for project in $(cat ~/repos); do
   echo $project
   destino=$(basename ${project})
-  mvn -q exec:java -Dexec.mainClass="br.edu.ifc.blumenau.analyzer.Main" -Dexec.args="$project" > "experimento2/$destino.log"
+  mvn -q exec:java -Dexec.mainClass="br.edu.ifc.blumenau.analyzer.Main" -Dexec.args="$project" > "$dir/$destino.log"
 done
